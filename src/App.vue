44d269f4a7389f078e8b67cbbe670b1d467eaef6
Olaya Sánchez <olaya.sanchez@vml.com>
2025-04-10 17:20:56 +0200

    Refactor nav into NavigationMenu component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
+import NavigationMenu from "./components/NavigationMenu.vue";
 </script>
 
 <template>
   <main class="container">
     <header>
-      <nav id="nav">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/brazil">Brazil</RouterLink>
-        <RouterLink to="/hawaii">Hawaii</RouterLink>
-        <RouterLink to="/panama">Panama</RouterLink>
-        <RouterLink to="/jamaica">Jamaica</RouterLink>
-      </nav>
+      <NavigationMenu />
     </header>
     <RouterView />
   </main>
---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <nav id="nav">
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/brazil">Brazil</RouterLink>
+    <RouterLink to="/hawaii">Hawaii</RouterLink>
+    <RouterLink to="/panama">Panama</RouterLink>
+    <RouterLink to="/jamaica">Jamaica</RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
