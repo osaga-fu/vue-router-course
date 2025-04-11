14ad68f687336362a5708ba940c4026318631f91
Olaya Sánchez <olaya.sanchez@vml.com>
2025-04-11 11:55:11 +0200

    Add name route to dynamic route

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,7 +11,7 @@ const destinations = sourceData.destinations;
       <RouterLink
         v-for="destination in destinations"
         :key="destination.id"
-        :to="destination.slug"
+        :to="{ name: 'destination.details', params: { id: destination.id } }"
       >
         <h2>
           {{ destination.name }}
