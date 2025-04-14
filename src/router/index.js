75aec881b265de74513fe0ddb6c4ebe935dbb19b
Olaya Sánchez <olaya.sanchez@vml.com>
2025-04-14 09:59:11 +0200

    Add route navigation guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue";
+import sourceData from "../data.json";
 
 const routes = [
   { path: "/", name: "Home", component: HomePage },
@@ -13,6 +14,19 @@ const routes = [
     name: "destination.details",
     component: () => import("../views/DestinationDetails.vue"),
     props: true,
+    beforeEnter: (to, from) => {
+      const exists = sourceData.destinations.find(
+        (destination) => destination.id === parseInt(to.params.id)
+      );
+      if (!exists)
+        return {
+          name: "NotFound",
+          // Allows keeping the URL while rendering a different page
+          params: { pathMatch: to.path.split("/").slice(1) },
+          query: to.query,
+          hash: to.hash,
+        };
+    },
     // NESTED ROUTE
     // children: [
     //   {
